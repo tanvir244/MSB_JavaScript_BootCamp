07ca17ea7dc319694f913a24cac8c9c2cc1613fc
Tanvir Rahman <109382396+tanvir244@users.noreply.github.com>
2024-09-14 02:16:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -112,3 +112,146 @@ console.log(user['name']);     // bracket notaion
 
 // Template Literals also support multi-line strings, making it easier to work with multiline text.
 // Template literals also allow for expressions and functions. Template literals provide a concise and readable way to work with strings and expressions in JavaScript.
+
+
+
+
+
+// ========================================================== Conditional Statements ==================================
+// ====================================================================================================================
+// Problem - 01: You need to determine whether a given year is a leap year or not.
+function leapYear(year) {
+    if(year % 400 === 0){
+        return true;
+    } else if(year % 100 === 0){
+        return true;
+    } else if(year % 4 === 0) {
+        return true;
+    }
+}
+console.log(leapYear(2020));
+console.log(leapYear(1900));
+console.log(leapYear(2000));
+// Concept: A leap year is divisible by 4, except if divisible by 100 unless also divisible by 400
+
+// Problem - 02: You are given three numbers. You need to determine whether you can form a valid triangle with these numbers as side lengths.
+function isValidTriangle(a, b, c){
+    if(a + b > c && b + c > a && c+a > b){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isValidTriangle(3, 4, 5));
+console.log(isValidTriangle(1, 2, 3));
+
+// Problem - 03: Write a function that takes a string representing a traffic light color ("red", "yellow", "green") and returns what action a driver should take (e.g., "Stop", "Slow down", "Go").
+function trafficLightAction(color) {
+    switch(color) {
+        case "red":
+            return "stop";
+        case "yellow":
+            return "Slow down";
+        case "green":
+            return "Go";
+        default:
+            return "Invalid color";
+    }
+}
+console.log(trafficLightAction("red"));
+console.log(trafficLightAction("green"));
+console.log(trafficLightAction("blue"));
+
+// Problem - 04: Write a function that takes two numbers and a string ("+", "-", "*", "/") representing an operation, and returns the result of that operation.
+function calculator(num1, num2, operation){
+    switch(operation) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num2 !== 0 ? num1 / num2 : "Cannot divide by Zero";
+        default:
+            return "Invalid operator";
+    }
+}
+console.log(calculator(5, 3, "/"));
+console.log(calculator(5, 0, "/"));
+
+// Problem - 04: Write a function that takes three numbers and returns the largest one using the ternary operator.
+function findLargerst(a, b, c) {
+    return a > b
+        ? (a > c ? a : c)
+        : (b > c ? b : c) 
+}
+console.log(findLargerst(3, 8, 5));
+
+// Problem - 05 : You are given a student's score and a boolean value indicating if the student has completed bonus assignments. Based on the score and bonus, determine the grade:
+// - If the score is 90 or above and the student completed the bonus, return "A+"
+// - If the score is 90 or above without bonus, return "A"
+// - If the score is between 80 and 89, return "B"
+// - If the score is between 70 and 79, return "C"
+// - If the score is between 60 and 69, return "D"
+// - Below 60, return "F"
+function getGrade(score, hasBonus){
+    return score >= 90 
+        ? (hasBonus ? 'A+' : 'A')
+        : score >= 80 
+            ? "B"
+            : score >= 70
+                ? "C"
+                : score >= 60
+                    ? "D"
+                    : "F";  
+}
+console.log(getGrade(95, false)); 
+console.log(getGrade(55, false)); 
+
+// Problem - 06: You need to determine the ticket price for a movie based on the customer's age and whether it’s a weekday or weekend. The price rules are:
+// - Children (under 12): $8 on weekdays, $10 on weekends.
+// - Seniors (65 and above): $6 on weekdays, $8 on weekends.
+// - Adults: $10 on weekdays, $12 on weekends.
+function getTicektPrice(age, isWeekend){
+    return age < 12 
+            ? (isWeekend ? 10 : 8)
+            : age >= 60
+                ? (isWeekend ? 8 : 6)
+                : (isWeekend ? 12 : 10);
+}
+console.log(getTicektPrice(10, true));
+// There is something mistake
+
+// Problem - 07 : Create a function that calculates the final price after applying discounts. The discounts are applied based on three conditions:
+// - Membership level: Gold members get a 20% discount, Silver members get 10%, and others get no discount.
+// - If the order is above $100, add an extra 5% discount.
+// - If it's Black Friday, add another 10% discount on top of everything else.
+function CalculateFinalPrice(price, membership, isBlackFriday) {
+    let discount = membership === "Gold"
+        ? 0.20
+        : membership === "Silver"
+            ? 0.10
+            : 0;
+
+    discount = discount + price > 100 ? 0.05 : 0;
+    discount = isBlackFriday ? 0.10 : 0;
+
+    return price - (price * discount);
+}
+console.log(CalculateFinalPrice(50, "Gold", false));
+
+// Problem - 08: Given three side lengths of a triangle, determine the type of triangle:
+// - If all sides are equal, it's an Equilateral triangle.
+// - If two sides are equal, it's an Isosceles triangle.
+// - If all sides are different, it's a Scalene triangle.
+// - If the side lengths don't form a valid triangle, return "Not a triangle." 
+function triangleType(a, b, c) {
+    return (a + b > c && b + c > a && c + a > b)
+        ? (a === b && b === c) ? "Equilateral triangle"
+            : (a === b || b === c || c === a) ? "Isosceles triagle"
+                : "Scalene triangle"
+        : "Not a triangle"
+} 
+console.log(triangleType(1, 2, 3));
+
